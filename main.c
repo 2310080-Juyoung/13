e29df6e67fdf8709c54013fbc040711a5972ab23
2310080-Juyoung <sophie4850@naver.com>
2023-11-30 15:15:10 +0900

    1-1

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct student {
+	int ID;//학번 
+	char name[100];//이름 
+	float score;//학점  
+};
+
+int main (void) {
+	
+struct student s1 = {132, "JuYeong", 4.3};
+	
+return 0;
+}
