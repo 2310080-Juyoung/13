0686c65a6375e2ee87c39d2c748c32075f85ea7b
2310080-Juyoung <sophie4850@naver.com>
2023-11-30 15:22:02 +0900

    1-2

--- main.c
+++ main.c
@@ -4,14 +4,23 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 struct student {
-	int ID;//학번 
-	char name[100];//이름 
-	float score;//학점  
-};
+	int ID;
+	char name[100];
+	float score;
+	};
+	
+void main (void) {
+	struct student s1 = {132, "JuYeong", 4.3};
+	
+	s1.ID = 2233;
+	s1.name[0] = 'c';
+	s1.score = 4.20000;
+	
+	printf("ID : %d\n", s1.ID);
+	printf("name : %s\n", s1.name);
+	printf("score : %f\n", s1.score);
+	
+	strcpy(s1.name,"JuYeop");
+	printf("name2: %s\n",s1.name);
+}	
 
-int main (void) {
-	
-struct student s1 = {132, "JuYeong", 4.3};
-	
-return 0;
-}
